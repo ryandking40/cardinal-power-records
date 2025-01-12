a8a798690cca4165e980c59395677b467baa1c72
ryandking40 <rking@usd356.org>
2025-01-12 08:47:02 -0600

    Run Build Fix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,10 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        snatched: ['var(--font-snatched)'],
+        'sport-solid': ['var(--font-sport-solid)']
+      },
     },
   },
   plugins: [],
